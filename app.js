c0bc3092dce12493c4c20646a2595e4187ea90cc
aoshuang <aoshuang@staff.sina.com.cn>
2018-10-23 23:14:32 +0800

    static

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 var express = require("express");
 var fs = require("fs");
+let path = require('path');
 var app = express();
 
-app.get("/" , function (req , res) {
-    fs.readFile("./project/index/static/index.html" , "utf-8" , function(err , data){
-        if(err){
-            res.send("<!DOCTYPE html><html><head><title>哈哈</title></head><body>123</body></html>");
-        }else{
-            res.send(data);
-        }
-    });
+// app.get("/" , function (req , res) {
+//     fs.readFile("./project/index/static/index.html" , "utf-8" , function(err , data){
+//         if(err){
+//             res.send("<!DOCTYPE html><html><head><title>哈哈</title></head><body>123</body></html>");
+//         }else{
+//             res.send(data);
+//         }
+//     });
+// });
+
+app.use(express.static(path.join(__dirname, './project/index/static')));
+
+
+app.all('/', function(req, res){
+    console.log("=======================================");
+    console.log("请求路径："+req.url);
+    var filename = req.url.split('/')[req.url.split('/').length-1];
+    var suffix = req.url.split('.')[req.url.split('.').length-1];
+    console.log("文件名：", filename);
+    if(req.url==='/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(get_file_content(path.join(__dirname, 'html', 'index.html')));
+    }else if(suffix==='css'){
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(get_file_content(path.join(__dirname, 'public', 'css', filename)));
+    }else if(suffix in ['gif', 'jpeg', 'jpg', 'png']) {
+        res.writeHead(200, {'Content-Type': 'image/'+suffix});
+        res.end(get_file_content(path.join(__dirname, 'public', 'images', filename)));
+    }
 });
 
 // // 网站首页接受 POST 请求
