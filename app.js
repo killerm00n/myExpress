f415e41965c41fcaa093d700ce93ae9987b2b04a
aoshuang <aoshuang@staff.sina.com.cn>
2018-10-31 15:01:45 +0800

    正文显示逻辑

--- app.js
+++ app.js
@@ -14,25 +14,27 @@ var app = express();
 // });
 
 app.use(express.static(path.join(__dirname, './project/index/static')));
+// 正文资源 请求返回
+app.use(express.static(path.join(__dirname, './project/index/src/articleAssets')));
 
 
-app.all('/', function(req, res){
-    console.log("=======================================");
-    console.log("请求路径："+req.url);
-    var filename = req.url.split('/')[req.url.split('/').length-1];
-    var suffix = req.url.split('.')[req.url.split('.').length-1];
-    console.log("文件名：", filename);
-    if(req.url==='/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(get_file_content(path.join(__dirname, 'html', 'index.html')));
-    }else if(suffix==='css'){
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.end(get_file_content(path.join(__dirname, 'public', 'css', filename)));
-    }else if(suffix in ['gif', 'jpeg', 'jpg', 'png']) {
-        res.writeHead(200, {'Content-Type': 'image/'+suffix});
-        res.end(get_file_content(path.join(__dirname, 'public', 'images', filename)));
-    }
-});
+// app.all('/', function(req, res){
+//     console.log("=======================================");
+//     console.log("请求路径："+req.url);
+//     var filename = req.url.split('/')[req.url.split('/').length-1];
+//     var suffix = req.url.split('.')[req.url.split('.').length-1];
+//     console.log("文件名：", filename);
+//     if(req.url==='/'){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.end(get_file_content(path.join(__dirname, 'html', 'index.html')));
+//     }else if(suffix==='css'){
+//         res.writeHead(200, {'Content-Type': 'text/css'});
+//         res.end(get_file_content(path.join(__dirname, 'public', 'css', filename)));
+//     }else if(suffix in ['gif', 'jpeg', 'jpg', 'png']) {
+//         res.writeHead(200, {'Content-Type': 'image/'+suffix});
+//         res.end(get_file_content(path.join(__dirname, 'public', 'images', filename)));
+//     }
+// });
 
 // // 网站首页接受 POST 请求
 // app.post('/', function (req, res) {
---
+++ project/index/src/components/article/article.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="page-article">
+        <section class="page-article_con" v-html="content"></section>
+    </section>
+</template>
+
+<script>
+import { markdown } from "markdown";
+import ajax from "./../../utils/ajax.js";
+export default {
+    data(){
+        return {
+            content:"",
+        }
+    },
+    created(){
+        let artId = this.$route.query.articleId
+        let _this = this;
+        ajax({
+            url: `/${artId}.md`,
+            type: 'get',
+            success(res){
+                console.log("正文res:" , res);
+                _this.content = markdown.toHTML(res);
+            }
+        })
+    },
+    mounted(){
+        
+    }
+}
+</script>
+
+<style scoped>
+    .page-article_con{
+        font-size:.3rem;
+    }
+</style>
